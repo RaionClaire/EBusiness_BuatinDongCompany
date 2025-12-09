027c92dd29228ef0b750166aea43d38c73e472c5
RaionClaire <chacastastaria@gmail.com>
2025-12-09 22:50:45 +0700

    Refactor preloader functionality and remove unused Firebase code

--- js/script.js
+++ js/script.js
@@ -166,48 +166,9 @@
 
   }); // End of a document
 
-  $(window).load(function(){
+  // Hide preloader when page is fully loaded
+  $(window).on('load', function(){
     $('.preloader').fadeOut();
   });
 
 })(jQuery);
-
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDmW7At0oMbq1YaqTKxhIoHY0Blg-X_smc",
-  authDomain: "buatindongcompany.firebaseapp.com",
-  projectId: "buatindongcompany",
-  storageBucket: "buatindongcompany.firebasestorage.app",
-  messagingSenderId: "205218732554",
-  appId: "1:205218732554:web:6f94ba6a82bfedd1305df3",
-  measurementId: "G-9CM9GN3F51"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
-
-
-const functions = require("firebase-functions");
-const stripe = require("stripe")("sk_test_...");
-
-exports.createCheckoutSession = functions.https.onRequest(async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: req.body.items,
-    mode: "payment",
-    success_url: "https://your-site.com/success",
-    cancel_url: "https://your-site.com/cancel",
-  });
-
-  res.json({ id: session.id });
-});
